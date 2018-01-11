e3f161487a2e5ef81307ac7b29749814d7f68a6c
hew <matt@hew.tools>
2018-01-10 21:19:39 -0800

    Adds keybase proof, copy changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => (
   <Container>
     <Box>
-      <H1 color={color.purple[0]}>Matthew Jones</H1>
-      <h2>engineer + designer</h2> 
-      <P w={[1, 1/2]}>ex-copywriter, marketer. passionate about javascript and the open web. mostly into react and functional stuff. i love hummus.</P>
+      <H1 color={color.purple[0]}>matthew jones</H1>
+      <h2>developer + designer</h2>
+      <P w={[1, 1 / 2]}>
+        ex-copywriter, marketer. passionate about javascript and the open web.
+        into react, functional stuff, and hummus.
+      </P>
       <p>
-        <a href='mailto:matt@hew.tools'>Get in touch</a><br />
+        <a href="https://twitter.com/tahini">get in touch</a>
+        <br />
       </p>
     </Box>
   </Container>
-)
+);
 
-export default Home
+export default Home;
