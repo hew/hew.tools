1600ccd66de50c26a70de94ec5a85bf72d2f5798
Matthew Jones <matt@klue.com>
2018-04-21 14:42:42 -0700

    Remove link.

--- src/data/index.js
+++ src/data/index.js
@@ -2,7 +2,6 @@ export default {
   siteTitle: 'hew.tools',
   headerLinks: [
     {text: '/', link: '/'},
-    {text: 'resume', link: '/resume'},
     {text: 'os', link: '/open-source'},
     {text: 'identity', link: '/identity'},
     // More pages...
