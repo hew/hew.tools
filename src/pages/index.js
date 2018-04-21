ba2dd86f507a50dfe4c710de48258cf1bb09d30a
Matthew Jones <matt@klue.com>
2018-04-21 14:41:00 -0700

    Update copy.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ const Home = () => (
   <Container>
     <Box>
       <H1 color={color.purple[0]}>matthew jones</H1>
-      <h2>product engineer</h2>
+      <h2>builds products</h2>
       <P w={[1, 1 / 2]}>
         CTO @ <a href="https://protohack.com">protohack</a> & <a href="https://startupleague.io">startup league</a>.
       </P>
