31654ec02a1574252605790dd8e8032a08b5cab3
Matthew Jones <matt@klue.com>
2018-05-08 02:52:08 -0700

    Changes folio link.

--- src/pages/identity.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-
-export default class Identity extends Component {
-  componentDidMount() {
-    window.location = 'https://keybase.io/tahini'
-  }
-  render () { return null }
-}
