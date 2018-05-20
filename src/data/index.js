21e126c69cd9209fcdd282faa003a1f1a7a0ff3d
Matthew Jones <matt@klue.com>
2018-05-20 08:22:35 -0700

    Adds expo link.

--- src/data/index.js
+++ src/data/index.js
@@ -13,7 +13,7 @@ export default {
         long:
           'Three words: <strong>no server fees</strong>. If the bulk of your website costs are going to hosting Wordpress, or paying for Squarespace, <a href="mailto:matt@hew.tools">we should chat</a>. In 2018, there is a better way. <a href="/serverless">read this </a> to learn more.'
       },
-      {short: 'react native', stack: ['expo', 'glamorous-native'], long: 'I\'m the co-founder and CTO of a real-time game with a RN front end. An expert with expo.io, I can translate mockups into reality with astounding speed. <a href="mailto:matt@hew.tools">let\'s chat</a>.'},
+      {short: 'react native', stack: ['expo', 'glamorous-native'], long: 'I\'m the co-founder and CTO of a real-time game with a RN front end. An expert with <a href="https://expo.io">expo.io</a>, I can translate mockups into reality with astounding speed. <a href="mailto:matt@hew.tools">let\'s chat</a>.'},
       {short: 'other', stack: [], long: 'Got a crazy idea? I might be interested. <a href="mailto:matt@hew.tools">let\'s chat</a>.'}
     ]
   }
