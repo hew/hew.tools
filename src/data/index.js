99a8dab42dd7252f60c549469f4e5e8f84e341c7
Matthew Jones <matt@klue.com>
2018-05-21 12:07:22 -0700

    Changes copy.

--- src/data/index.js
+++ src/data/index.js
@@ -11,9 +11,9 @@ export default {
         short: 'static CMS',
         stack: ['contentful', 'gatsbyjs', 'netlify'],
         long:
-          'Three words: <strong>no server fees</strong>. If the bulk of your website costs are going to hosting Wordpress, or paying for Squarespace, <a href="mailto:matt@hew.tools">we should chat</a>. In 2018, there is a better way. <a href="/serverless">read this </a> to learn more.'
+          'Three words: <strong>no server fees</strong>. If the bulk of your website costs are going to hosting Wordpress, or paying for Squarespace, <a href="mailto:matt@hew.tools">we should chat</a> about a static site CMS, otherwise known as the JAM Stack. Check out my <a href="/serverless">my serverless page</a> to learn more.'
       },
-      {short: 'react native', stack: ['expo', 'glamorous-native'], long: 'I\'m the co-founder and CTO of a real-time game with a RN front end. An expert with <a href="https://expo.io">expo.io</a>, I can translate mockups into reality with astounding speed. <a href="mailto:matt@hew.tools">let\'s chat</a>.'},
+      {short: 'react native', stack: ['expo', 'glamorous-native'], long: 'I\'m the co-founder and CTO of a real-time game with a React Native front end. The advantage of RN, and frameworks like <a href="https://expo.io">expo.io</a>, is <em>speed</em>. If you have an idea for an app, let\'s make it happen. <a href="mailto:matt@hew.tools">let\'s chat</a>.'},
       {short: 'other', stack: [], long: 'Got a crazy idea? I might be interested. <a href="mailto:matt@hew.tools">let\'s chat</a>.'}
     ]
   }
