a46580110047fcea2ac6b65ad16b6e1f010378c1
Matthew Jones <matt@hew.tools>
2018-05-23 13:40:59 -0700

    Alters copy for previous commit.

--- src/data/index.js
+++ src/data/index.js
@@ -9,12 +9,19 @@ export default {
     expertise: [
       {
         short: 'static CMS',
-        stack: ['contentful', 'gatsbyjs', 'netlify'],
         long:
-          'Three words: <strong>no server fees</strong>. If the bulk of your website costs are going to hosting Wordpress, or paying for Squarespace, <a href="mailto:matt@hew.tools">we should chat</a> about a static site CMS, otherwise known as the JAM Stack. Check out my <a href="/serverless">my serverless page</a> to learn more.'
+          'Three words: <strong>no server fees</strong>. If the bulk of your website costs are going to hosting Wordpress, or paying for Squarespace, <a href="mailto:matt@hew.tools">we should chat</a> about a static site CMS, otherwise known as the JAM Stack. Check out my <a href="/static">my static cms page</a> to learn more.'
       },
-      {short: 'react native', stack: ['expo', 'glamorous-native'], long: 'I\'m the co-founder and CTO of a real-time game with a React Native front end. The advantage of RN, and frameworks like <a href="https://expo.io">expo.io</a>, is <em>speed</em>. If you have an idea for an app, let\'s make it happen. <a href="mailto:matt@hew.tools">let\'s chat</a>.'},
-      {short: 'other', stack: [], long: 'Got a crazy idea? I might be interested. <a href="mailto:matt@hew.tools">let\'s chat</a>.'}
+      {
+        short: 'react native',
+        long:
+          'I\'m the co-founder and CTO of a real-time game with a React Native front end. The advantage of RN, and frameworks like <a href="https://expo.io">expo.io</a>, is <em>speed</em>. If you have an idea for an app, let\'s make it happen. <a href="mailto:matt@hew.tools">let\'s chat</a>.'
+      },
+      {
+        short: 'react / other',
+        long:
+          'Got a crazy idea that requires a javascript specialist? I might be interested. <a href="mailto:matt@hew.tools">let\'s chat</a>.'
+      }
     ]
   }
 };
