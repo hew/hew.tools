7e01d9310e44b43c61dc1ca87ffac5e4336b2819
hew <mathujones@gmail.com>
2018-10-01 16:34:25 -0700

    Updates.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const Home = () => (
         matthew jones
       </H1>
       <h2 w={[1, 1 / 2]}>javascript and reasonml generalist</h2>
-      <P mt={4} w={[1, 2 / 3]}>
+      <P mt={4} mb={5} w={[1, 2 / 3]}>
         cto @{' '}
         <a href="https://startupleague.io">startupleague.io</a>. hummus, basketball,
         and bad jokes, all the way down. hailing from Vancouver, Canada. If you want, check out some{' '}
